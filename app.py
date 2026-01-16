159e818e68d2ab36d8d6944c4f6c393c1cea6078
Ryan <usouniversitario780@gmail.com>
2026-01-16 17:32:05 -0600

    Comit de prueba en nueva rama

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print ('Hola desde VS Code y Ubuntu')
 print("Hola Ryan, este es el segundo commit desde VS Code en Ubuntu")
 print("Esta línea fue agregada en la rama nueva-rama")
+print("Probando cambios en la rama nueva-rama")
